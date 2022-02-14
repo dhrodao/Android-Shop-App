54bcab685a7e3b8af8ca0f7044d936804854e82e
Daniel Hervás Rodao <danielhervasrodao@gmail.com>
2022-02-14 12:22:38 +0100

    Implemented state restoring for RecyclerView and fixed state restoring for SeekBar

--- app/src/main/java/com/dhrodao/androidfruitselector/MainActivity.kt
+++ app/src/main/java/com/dhrodao/androidfruitselector/MainActivity.kt
@@ -2,6 +2,9 @@ package com.dhrodao.androidfruitselector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -28,15 +31,47 @@ open class MainActivity : AppCompatActivity() {
     private var fruitQuantity : Int = 0
     private var basketPrice : Double = 0.00
 
+    private var onRestore : Boolean = false
+    private lateinit var restoredListContent : ArrayList<BasketItem>
+
     private var basketItems : ArrayList<BasketItem> = ArrayList()
 
     private lateinit var customRecyclerAdapter : CustomRecyclerAdapter // Dynamic list so need to access from methods
 
-    data class BasketItem(val fruit : String, val icon : Int, val quantity : Int)
+    // Needs to inherit from parcelable in order to persist views when screen changes
+    data class BasketItem(val fruit: String?, val icon: Int, val quantity: Int) : Parcelable {
+        constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readInt()
+        )
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeString(fruit)
+            parcel.writeInt(icon)
+            parcel.writeInt(quantity)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<BasketItem> {
+            override fun createFromParcel(parcel: Parcel): BasketItem {
+                return BasketItem(parcel)
+            }
+
+            override fun newArray(size: Int): Array<BasketItem?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("FruitSelector", "onCreate")
 
         initComponents() // initialize late init variables
 
@@ -65,6 +100,42 @@ open class MainActivity : AppCompatActivity() {
                 resetDefaultValues()
             }
         }
+    }
+
+    override fun onRestoreInstanceState(outState: Bundle) {
+        super.onRestoreInstanceState(outState)
+        Log.d("FruitSelector", "onRestoreInstanceState")
+        onRestore = true
+        outState.let {
+            it.getParcelableArrayList<BasketItem>("basketRecyclerViewItems").also { restoredParcelable ->
+                if (restoredParcelable != null) {
+                    restoredListContent = restoredParcelable
+                    for (item in restoredListContent){
+                        val index = basketItems.size
+                        basketItems.add(item)
+                        customRecyclerAdapter.notifyItemInserted(index)
+                    }
+                }
+            }
+            it.getInt("currentSpinnerItem").also { selection -> spinner.setSelection(selection) }
+            it.getDouble("fruitPrice").also { price -> fruitPrice = price }
+            it.getDouble("computedFruitPrice").also { computed -> computedFruitPrice = computed }
+            it.getDouble("basketPrice").also { price -> basketPrice = price }
+            it.getInt("fruitQuantity").also { quantity -> fruitQuantity = quantity }
+            it.getInt("currentProgress").also { progress -> seekBar.progress = progress }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putParcelableArrayList("basketRecyclerViewItems", basketItems)
+        outState.putInt("currentSpinnerItem", spinner.selectedItemPosition)
+        outState.putDouble("fruitPrice", fruitPrice)
+        outState.putDouble("computedFruitPrice", computedFruitPrice)
+        outState.putDouble("basketPrice", basketPrice)
+        outState.putInt("fruitQuantity", fruitQuantity)
+        outState.putInt("currentProgress", seekBar.progress)
     }
 
     private fun initComponents() {
@@ -130,12 +201,12 @@ open class MainActivity : AppCompatActivity() {
                                         private val fruits : Array<FruitItems>
     ) : AdapterView.OnItemSelectedListener {
         private var fruitText : String = ""
+        private var prevPosition : Int? = null
 
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            fruitQuantity = 0
-            seekBar.progress = 0
-            fruitText = parent?.findViewById<TextView>(R.id.text).let { it?.text }.toString()
+            resetData(position)
 
+            fruitText = parent?.findViewById<TextView>(R.id.text).let { it?.text }.toString()
             currentFruitItem = getFruitItem()
 
             if (position == 0) {
@@ -148,6 +219,17 @@ open class MainActivity : AppCompatActivity() {
         }
 
         override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+        private fun resetData(position: Int) {
+            prevPosition?.let { if (it != position) onRestore = false }
+
+            if (!onRestore) {
+                fruitQuantity = 0
+                seekBar.progress = 0
+            }
+
+            prevPosition = position
+        }
 
         private fun setVisibility(viewsAffected: Array<View>, visibility : Int) {
             for (view in viewsAffected) {
